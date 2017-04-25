6d0fb3dfe2701d76f5551088661e1466aa480d97
Simon <simon.curran.89@gmail.com>
2017-04-25 21:35:45 +1000

    firebase auth problem

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,56 +1,310 @@
-angular.module('starter.controllers', [])
+angular.module('yourAppsName.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', ['$scope', 'modalService', 'userService',
+  function($scope, modalService, userService) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    $scope.modalService = modalService;
 
-  // Form data for the login modal
-  $scope.loginData = {};
+    $scope.logout = function () {
+      userService.logout();
+    };
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+}])
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+.controller('MyStocksCtrl', ['$scope', 'myStocksArrayService', 'stockDataService', 'stockPriceCacheService',
+  function($scope, myStocksArrayService, stockDataService, stockPriceCacheService) {
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+    $scope.$on("$ionicView.afterEnter", function() {
+      $scope.getMyStocksData();
+    });
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+    $scope.getMyStocksData = function () {
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
+      myStocksArrayService.forEach(function(stock) {
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+        var promise = stockDataService.getPriceData(stock.ticker);
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+        $scope.myStocksData =[];
+
+        promise.then(function(data) {
+          $scope.myStocksData.push(stockPriceCacheService.get(data.symbol));
+        });
+      });
+
+      $scope.$broadcast('scroll.refreshComplete');
+    };
+
+    $scope.unfollowStock = function (ticker) {
+      followStockService.unfollow(ticker);
+      $scope.getMyStocksData();
+    };
+
+}])
+
+.controller('StockCtrl', ['$scope', '$stateParams', '$window', '$ionicPopup', 'followStockService', 'stockDataService', 'dateService', 'chartDataService', 'notesService', 'newsService',
+  function($scope, $stateParams, $window, $ionicPopup, followStockService, stockDataService, dateService, chartDataService, notesService, newsService) {
+
+    $scope.ticker = $stateParams.stockTicker;
+    $scope.chartView = 4;
+    $scope.oneYearAgoDate = dateService.oneYearAgoDate();
+    $scope.todayDate = dateService.currentDate();
+    $scope.following = followStockService.checkFollowing($scope.ticker);
+
+    $scope.stockNotes = [];
+
+    $scope.$on("$ionicView.afterEnter", function() {
+      getPriceData();
+      getDetailsData();
+      getChartData();
+      getNews();
+      $scope.stockNotes = notesService.getNotes($scope.ticker);
+    });
+
+    $scope.toggleFollow = function () {
+      if($scope.following) {
+        followStockService.unfollow($scope.ticker);
+        $scope.following = false;
+      }
+      else {
+        followStockService.follow($scope.ticker);
+        $scope.following = true;
+      }
+    };
+
+    $scope.openWindow = function (link) {
+      // TODO install and set up inAppBrowswer
+      console.log("openWindow -> " + link);
+    };
+
+    $scope.chartViewFunc = function(n) {
+      $scope.chartView = n;
+    };
+
+    $scope.addNote = function() {
+      $scope.note = {title: 'Note', body: '', date: $scope.todayDate, ticker: $scope.ticker};
+
+      var note = $ionicPopup.show({
+        template: '<input type="text" ng-model="note.title" id="stock-note-tile"><textarea type="text" ng-model="note.body" id="stock-note-body"></textarea>',
+        title: 'New Note for ' + $scope.ticker,
+        scope: $scope,
+        buttons: [
+          {
+            text: 'Cancel',
+            onTap: function(e) {
+              return;
+            }
+          },
+          {
+            text: '<b>Save</b>',
+            type: 'button-balanced',
+            onTap: function(e) {
+              notesService.addNote($scope.ticker, $scope.note);
+            }
+          }
+        ]
+      });
+      note.then(function(res) {
+        $scope.stockNotes = notesService.getNotes($scope.ticker);
+      });
+    };
+
+    $scope.openNote = function(index, title, body) {
+      $scope.note = {title: title, body: body, date: $scope.todayDate, ticker: $scope.ticker};
+
+      var note = $ionicPopup.show({
+        template: '<input type="text" ng-model="note.title" id="stock-note-title"><textarea type="text" ng-model="note.body" id="stock-note-body"></textarea>',
+        title: $scope.note.title,
+        scope: $scope,
+        buttons: [
+          {
+            text: 'Delete',
+            type: 'button-assertive button-small',
+            onTap: function(e) {
+              notesService.deleteNote($scope.ticker, index);
+            }
+          },
+          {
+            text: 'Cancel',
+            type: 'button-small',
+            onTap: function(e) {
+              return;
+            }
+          },
+          {
+            text: '<b>Save</b>',
+            type: 'button-balanced button-small',
+            onTap: function(e) {
+              notesService.deleteNote($scope.ticker, index);
+              notesService.addNote($scope.ticker, $scope.note);
+            }
+          }
+        ]
+      });
+      note.then(function(res) {
+        $scope.stockNotes = notesService.getNotes($scope.ticker);
+      });
+    };
+
+    function getNews() {
+      $scope.newsStories = [];
+
+      var promise = newsService.getNews($scope.ticker);
+
+      promise.then(function(data) {
+        $scope.newsStories = data;
+      });
+    }
+
+
+
+    function getPriceData() {
+      var promise = stockDataService.getPriceData($scope.ticker);
+
+      promise.then(function(data) {
+        $scope.stockPriceData = data;
+
+        if(data.chg_percent >= 0 && data !== null) {
+          $scope.reactiveColor = {'background-color': '#33cd5f', 'border-color': 'rgba(255,255,255,.3)'};
+        }
+        else if (data.chg_percent < 0 && data !== null) {
+          $scope.reactiveColor = {'background-color': '#ef473a', 'border-color': 'rgba(0,0,0,.2)'};
+        }
+
+      });
+    }
+
+    function getDetailsData() {
+      var promise = stockDataService.getDetailsData($scope.ticker);
+
+      promise.then(function(data) {
+        $scope.stockDetailsData = data;
+
+      });
+
+    }
+
+    function getChartData() {
+      var promise = chartDataService.getHistoricalData($scope.ticker, $scope.oneYearAgoDate, $scope.todayDate);
+      promise.then(function(data){
+
+        $scope.myData = JSON.parse(data)
+          .map(function(series) {
+            series.values = series.values.map(function(d) { return {x: d[0], y: d[1] }; });
+            return series;
+          });
+
+      });
+    }
+
+
+    	var xTickFormat = function(d) {
+    		var dx = $scope.myData[0].values[d] && $scope.myData[0].values[d].x || 0;
+    		if (dx > 0) {
+          return d3.time.format("%b %d")(new Date(dx));
+    		}
+    		return null;
+    	};
+
+      var x2TickFormat = function(d) {
+        var dx = $scope.myData[0].values[d] && $scope.myData[0].values[d].x || 0;
+        return d3.time.format('%b %Y')(new Date(dx));
+      };
+
+      var y1TickFormat = function(d) {
+        return d3.format(',f')(d);
+      };
+
+      var y2TickFormat = function(d) {
+        return d3.format('s')(d);
+      };
+
+      var y3TickFormat = function(d) {
+        return d3.format(',.2s')(d);
+      };
+
+      var y4TickFormat = function(d) {
+        return d3.format(',.2s')(d);
+      };
+
+      var xValueFunction = function(d, i) {
+        return i;
+      };
+
+      var marginBottom = ($window.innerWidth / 100) * 10;
+
+    	$scope.chartOptions = {
+        chartType: 'linePlusBarWithFocusChart',
+        data: 'myData',
+        margin: {top: 15, right: 10, bottom: marginBottom, left: 10},
+        interpolate: "cardinal",
+        useInteractiveGuideline: false,
+        yShowMaxMin: false,
+        xShowMaxMin: false,
+        showLegend: false,
+        useVoronoi: false,
+        tooltips: false,
+        xValue: xValueFunction,
+        xAxisTickFormat: xTickFormat,
+        x2AxisTickFormat: x2TickFormat,
+        y1AxisTickFormat: y1TickFormat,
+        y2AxisTickFormat: y2TickFormat,
+        y3AxisTickFormat: y3TickFormat,
+        y4AxisTickFormat: y4TickFormat,
+        transitionDuration: 500,
+        y1AxisLabel: 'Price',
+        y3AxisLabel: 'Volume',
+        noData: 'Loading Data...'
+    	};
+
+}])
+
+.controller('SearchCtrl', ['$scope', '$state','modalService', 'searchService',
+  function($scope, $state, modalService, searchService) {
+
+    $scope.closeModal = function () {
+      modalService.closeModal();
+    };
+
+    $scope.search = function() {
+      $scope.searchResults = '';
+      startSearch($scope.searchQuery);
+    };
+
+    var startSearch = ionic.debounce(function (query) {
+      searchService.search(query)
+        .then(function (data) {
+          $scope.searchResults = data;
+        });
+    }, 750);
+
+    $scope.goToStock = function (ticker) {
+      modalService.closeModal();
+      $state.go('app.stock', {stockTicker: ticker});
+    };
+
+}])
+
+
+.controller('LoginSignupCtrl', ['$scope', 'modalService', 'userService',
+  function ($scope, modalService, userService) {
+
+    $scope.user = {email: '', password: ''};
+
+    $scope.closeModal = function () {
+      modalService.closeModal();
+    };
+
+    $scope.signup = function (user) {
+      userService.signup(user);
+    };
+
+    $scope.login = function (user) {
+      userService.login(user);
+    };
+
+  }])
+
+
+
+
+;
---
+++ www/js/directives.js
@@ -0,0 +1,2 @@
+angular.module('yourAppsName.directives', [])
+;
